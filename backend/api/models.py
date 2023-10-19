94a4f53b6298ed3bedda3918dd0d5820d9449e13
Yulin Zheng <lyra1225@gmail.com>
2023-10-18 22:48:27 -0700

    add notes model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.TextField(null=True, blank=True)
+    body = models.TextField(null=True)
+    url = models.URLField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.body[0:500]

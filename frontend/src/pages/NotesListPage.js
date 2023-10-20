4dedf3b4cbcf45ad2abaafc75bdeeb4af8db8b8d
Yulin Zheng <lyra1225@gmail.com>
2023-10-20 16:59:12 -0700

    add notes page

---
+++ frontend/src/pages/NotesListPage.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react'
+
+const NotesListPage = () => {
+    let [notes, setNotes] = useState([])
+
+    useEffect(() => {
+        getNotes()
+    }, [])
+
+    let getNotes = async () => {
+        let response = await fetch('http://localhost:8000/api/notes/')
+        let data = await response.json()
+        setNotes(data)
+    }
+
+    return (
+        <div>
+            notes
+        </div>
+    )
+}
+
+export default NotesListPage
